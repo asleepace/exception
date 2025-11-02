80b7669c313a119f0c23d587be9ba283d24fc837
Colin <colin_teahan@yahoo.com>
2025-11-02 02:55:31 -0800

    scoped index is now working well

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,9 @@
 export type ExcpClass = typeof Exception
 export type ExcpInstance = InstanceType<ExcpClass>
 
-/**
- * Base exception class which all other errors inherit from.
- */
-export class Exception extends Error {
-  static readonly scope: string = 'global'
-
-  /**
-   * If the provided argument e is an instance of this class,
-   * then the callback will be triggerd.
-   */
-  public static match<T>(e: unknown): e is ExcpInstance
-  public static match<T>(
-    e: unknown,
-    callback: (err: ExcpInstance) => T
-  ): T | void
-  public static match<T>(e: unknown, callback?: (err: ExcpInstance) => T): any {
-    if (e instanceof Exception) {
-      if (!callback) return true
-      return callback?.(e as any)
-    } else {
-      if (callback) return undefined
-      return false
-    }
-  }
-
-  /**
-   * Check if the provided object is an instance of this class.
-   */
-  public static is(obj: unknown): obj is ExcpInstance {
-    return obj instanceof Exception
-  }
-
-  /**
-   * Shorthand for creating a new instance and throwing.
-   */
-  public static throw(...args: any[]): never {
-    throw new this(...args)
-  }
-
-  public code: number = 0
-  public readonly scope: string = 'global'
-
-  constructor(...args: any[]) {
-    super(safeEncode(...args))
-  }
-
-  public debug(): this {
-    console.log({
-      name: this.name,
-      message: this.message,
-      code: this.code,
-    })
-    return this
-  }
+const getScopedKey = (scope: string, excepName: string) => {
+  return [...new Set(`${scope}:${excepName}`.split(':'))].join(':')
 }
-
-const MAX_ERROR_DEFS = 100
-const DEFAULT_SCOPE = 'global'
-const RANGE_SIZE = 1000
 
 function safeEncode(...args: any[]): string {
   try {
@@ -75,13 +19,95 @@ function safeEncode(...args: any[]): string {
   }
 }
 
-function getScopeBadge(scope: string): string {
-  let sanitized = scope.replace('global', '')
-  if (sanitized === ':') return ''
-  if (sanitized.startsWith(':')) {
-    sanitized = sanitized.slice(1)
+/**
+ * Base exception class which all other errors inherit from.
+ */
+export class Exception extends Error {
+  static readonly MAX_SCOPED_DEFS: number = 100
+  static readonly key: string = getScopedKey('global', Exception.name)
+  static readonly scope: string = 'global'
+  static readonly scopeIndex: number = 0 // Add static scopeIndex
+
+  /**
+   * Global scope which contains all error definitions.
+   */
+  static readonly globalRegistry = new Set<ExcpClass>()
+
+  static keys(): string[] {
+    return Array.from(this.globalRegistry).map((excp) => excp.key)
   }
-  return `[${sanitized}]`
+
+  static forKey(key: string): ExcpClass | undefined {
+    const entries = Array.from(this.globalRegistry)
+    return entries.find((excp) => excp.key === key)
+  }
+
+  /**
+   * Check if given argument is an instance of this class and either
+   * perform one of the following:
+   *
+   * ```ts
+   * const example1: boolean = Exception.match(e)
+   * const example2: number = Exception.match(e, () => 123)
+   * const example3: string = Exception.natch(e, (err) => err.message)
+   * ```
+   */
+  public static match<T>(e: unknown): e is ExcpInstance
+  public static match<T>(e: unknown, fn: (err: ExcpInstance) => T): T | void
+  public static match<T>(e: unknown, fn?: (err: ExcpInstance) => T): any {
+    const isInstanceType = e instanceof this
+    if (typeof fn === 'function') {
+      if (!isInstanceType) return false
+      return fn(e)
+    } else {
+      return isInstanceType
+    }
+  }
+
+  /**
+   * Check if the provided object is an instance of this class.
+   */
+  public static is(obj: unknown): obj is ExcpInstance {
+    return obj instanceof Exception
+  }
+
+  /**
+   * Shorthand for creating a new instance and throwing.
+   */
+  public static throw(...args: any[]): never {
+    throw this.new(...args)
+  }
+
+  public static new(...args: any[]): Exception {
+    return new this(...args)
+  }
+
+  public override readonly name: string = 'Exception'
+  public readonly scope: string = 'global'
+  public get scopeIndex(): number {
+    return (this.constructor as typeof Exception).scopeIndex
+  }
+
+  constructor(...args: any[]) {
+    super(safeEncode(...args))
+  }
+
+  public debug({ verbose = false } = {}): this {
+    const entries: Record<string, any> = {
+      name: this.name,
+      message: this.message,
+      scope: this.scope,
+      scopeIndex: this.scopeIndex,
+    }
+    if (verbose) {
+      entries['timestamp'] = new Date().toLocaleString('en-US', {
+        dateStyle: 'short',
+        timeStyle: 'long',
+      })
+    }
+    console.log(entries) // NOTE: keep this here!
+    return this
+  }
 }
 
 /**
@@ -89,40 +115,63 @@ function getScopeBadge(scope: string): string {
  */
 export const err = {
   Exception,
-  definitions: new Set<ExcpClass>(),
-  getScopeKeys() {
-    return new Set([...this.definitions].map((excp) => excp.scope))
-  },
-  entries() {
-    const values = Array.from(this.definitions.values())
-    return values.map((excp) => {
-      return [excp.scope, excp] as const
-    })
-  },
-  add(excp: ExcpClass) {
-    this.definitions.add(excp)
-  },
-  enum<Keys extends string[] = string[]>(options: { scope?: string } = {}) {
-    const scope = options.scope ?? 'global'
+  enum<Keys extends string[] = string[]>(options = { scope: 'global' }) {
+    const SCOPE_NAME = options.scope
     return new Proxy([], {
-      get: (target, name) => {
-        if (typeof name === 'symbol') return target[name as any]
-        if (name === 'length') return MAX_ERROR_DEFS
-
-        const errorName = String(name)
-        const errorCode = this.getScopeKeys().size + 1
+      get: (target, errorName) => {
+        /**
+         * Handle special properties here...
+         */
+        if (typeof errorName === 'symbol') return target[errorName as any]
+        if (errorName === 'length') return Exception.MAX_SCOPED_DEFS
 
         /**
-         * Custom exception.
+         * Unique key which is used to register errors.
+         */
+        const KEY = getScopedKey(options.scope, errorName)
+
+        // Return existing if already defined
+        const existing = Exception.forKey(KEY)
+        if (existing) return existing
+
+        // Calculate scopeIndex BEFORE creating the class
+        const scopedErrors = Exception.keys().filter((key) =>
+          key.startsWith(options.scope)
+        )
+        const nextIndex = scopedErrors.length + 1
+
+        /**
+         * ## Scoped Exception
+         *
+         * Custom error definition which extends the base `Exception` type or
+         * the provided exception.
          */
         class ScopedException extends Exception {
-          public readonly scopeName: string = scope
-          // static helpers...
-          static override match<T>(
+          /**
+           * Unique identifier which is used to filter definitions.
+           */
+          static override key: string = KEY
+          static override scope: string = options.scope
+          static override scopeIndex: number = nextIndex // Assign unique index
+
+          /**
+           * If the provided argument e is an instance of this class,
+           * then the callback will be triggerd.
+           */
+          public static override match<T>(e: unknown): e is ScopedException
+          public static override match<T>(
             e: unknown,
-            callback?: (err: ExcpInstance) => T
-          ): T | void {
-            if (e instanceof ScopedException) return callback?.(e as any)
+            fn: (err: ScopedException) => T
+          ): T | false
+          public static override match<T>(
+            e: unknown,
+            fn?: (err: ScopedException) => T
+          ): any {
+            if (e instanceof ScopedException) {
+              return fn?.(e as any) ?? true
+            } else {
+              return fn ? undefined : false
+            }
           }
 
           static override is(
@@ -137,25 +186,27 @@ export const err = {
 
           // instance properties ...
 
-          override name: string = errorName
-          override code: number = errorCode
-          public override readonly scope: string = scope
+          public override name: string = String(errorName)
+          public override scope: string = options.scope
+          public override get scopeIndex() {
+            return nextIndex
+          }
 
           constructor(...args: any[]) {
             super(...args)
-            let msg = this.name.trim()
-            if (this.scope && this.scope !== 'global') {
-              msg = `[${this.scope.replaceAll('global:', '')}] ${msg}`
-            }
-            if (this.message) {
-              msg += `: ${this.message}`
-            }
-
-            this.message = msg + ` (code: ${this.code})`
+            // format message like so:
+            // TestError (code: 2)
+            // TestError (code: 2): A message.
+            const components: string[] = []
+            this.scope !== 'global' && components.push(`[${this.scope}] `)
+            components.push(this.name)
+            this.message && components.push(`: ${this.message}`)
+            // NOTE: Important to set the message here.
+            this.message = components.join('')
           }
         }
-
-        this.definitions.add(ScopedException)
+        // NOTE: make sure to add definition to global registry
+        Exception.globalRegistry.add(ScopedException)
         return ScopedException
       },
     }) as unknown as {
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -6,39 +6,53 @@ import { err } from '@/index'
 // ================================================================================
 
 describe('Basic', () => {
-  it('should be able to define basic errors', () => {
+  it('1. should be able to define basic errors', () => {
     const { BasicError } = err.enum()
     const error = new BasicError('test message')
+    expect(BasicError.key).toBe('global:BasicError')
     expect(error).toBeInstanceOf(BasicError)
     expect(error).toBeInstanceOf(err.Exception)
-    expect(error.message).toBe('BasicError: test message (code: 1)')
-    expect(error.code).toBe(1)
+    expect(error.message).toBe('BasicError: test message')
   })
 
-  it('should be able to define with scopes', () => {
+  it('2. should be able to define with scopes', () => {
     const { NormalError } = err.enum()
     const { ScopedError } = err.enum({ scope: 'test-scope' })
     const normal = new NormalError('global scope')
     const scoped = new ScopedError('custom scope')
     expect(normal.scope).toBe('global')
-    expect(normal.code).toBe(2) // NOTE: this is global btw
     expect(scoped.scope).toBe('test-scope')
-    expect(scoped.code).toBe(2)
   })
 
-  it('should display name in message properly', () => {
+  it('3. should display name in message properly', () => {
     const { BasicError } = err.enum()
     const e1 = new BasicError('with message')
     const e2 = new BasicError() // without message
-    expect(e1.message).toBe('BasicError: with message (code: 2)')
-    expect(e2.message).toBe('BasicError (code: 2)') // should just be name
+    expect(e1.message).toBe('BasicError: with message')
+    expect(e2.message).toBe('BasicError') // should just be name
     const { ScopeError } = err.enum({
       scope: 'main-test',
     })
     const e3 = new ScopeError('with message')
     const e4 = new ScopeError()
-    expect(e3.message).toBe('[main-test] ScopeError: with message (code: 2)')
-    expect(e4.message).toBe('[main-test] ScopeError (code: 2)')
+    expect(e3.message).toBe('[main-test] ScopeError: with message')
+    expect(e4.message).toBe('[main-test] ScopeError')
+  })
+
+  it('4. should be debuggable', () => {
+    const { BasicError } = err.enum()
+    const { ScopedError } = err.enum({ scope: 'basic-debug' })
+    const e5 = new BasicError('debug me')
+    expect(e5.debug()).toBe(e5)
+    const e6 = new ScopedError('another one')
+    expect(e6.debug({ verbose: true })).toBe(e6)
+  })
+
+  it('5. should index properly', () => {
+    const { Order1, Order2, Order3 } = err.enum({ scope: 'order-test' })
+    expect(Order1.new().scopeIndex).toBe(1)
+    expect(Order2.new().scopeIndex).toBe(2)
+    expect(Order3.new().scopeIndex).toBe(3)
   })
 })
 
@@ -62,26 +76,33 @@ describe('Static Methods', () => {
       throw err1
     } catch (e) {
       if (TestError.is(e)) {
-        expect(e.message).toBe('TestError (code: 2)')
+        expect(e.message).toBe('TestError')
       }
     }
   })
 
   it('3. can use static is method as type-guard ', () => {
     const { TestError } = err.enum({ scope: 'static-3' })
-    const err1 = new TestError()
-    try {
-      throw err1
-    } catch (e) {
-      expect(TestError.match(e)).toBeTrue()
-      expect(TestError.match(e, () => 123)).toBe(123)
-      // just to check the types
-      const output = TestError.match(e, (err) => {
-        expect(err.scope).toBe('static-3')
-        return 'ok'
-      })
+    const e = new TestError() as unknown
+    expect(TestError.match(e)).toBe(true) // use as type guard
+    expect(TestError.match(e, () => 123)).toBe(123) // use to return
+    expect(TestError.match(null)).toBe(false)
+    expect(TestError.match(null, () => true)).toBe(undefined)
+  })
+})
 
-      expect(output).toBe('ok')
-    }
+// ================================================================================
+//  Edge Cases
+// ================================================================================
+
+describe('Edge Cases', () => {
+  it('1. should not throw on infinite extraction.', () => {
+    const { Err, ...infinite } = err.enum({ scope: 'edge-1' })
+    expect(infinite).toBeObject()
+  })
+
+  it('2. handle weird scopes.', () => {
+    const { Err } = err.enum({ scope: 'global:global:global' })
+    expect(Err.key).toBe('global:Err')
   })
 })
